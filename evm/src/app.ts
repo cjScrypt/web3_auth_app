704a7aea17cf9bcf1c0e51d50e03a90674397cc7
Electrifier <samchijioke16@gmail.com>
2025-04-17 12:34:41 +0100

    feat: Add `authRouter` to main express app

--- evm/src/app.ts
+++ evm/src/app.ts
@@ -1,4 +1,5 @@
 import { Express } from "express";
+import { authRouter } from "./api/routers";
 import { AppMiddleware } from "./middlewares";
 
 export const configureApp = (app: Express) => {
@@ -7,4 +8,6 @@ export const configureApp = (app: Express) => {
     app.use(AppMiddleware.addCorsHeaderToResponse);
 
     app.use(AppMiddleware.addUserToRequestFromAuth);
+
+    app.use("auth", authRouter);
 }
