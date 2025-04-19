ae7407876198afffc17cf3c376dbcadc1be2d5a8
Electrifier <samchijioke16@gmail.com>
2025-04-19 23:58:24 +0100

    chore: Apply validation middleware to 'generatePayload' and 'signinEVM' routes

--- src/api/routers/auth.router.ts
+++ src/api/routers/auth.router.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import { AuthController } from "../controllers";
 import { AppMiddleware } from "../../middlewares";
-import { CheckProofDto } from "../../types";
+import { CheckProofDto, GeneratePayloadDto, WalletSignInDto } from "../../types";
 
 export const authRouter = Router();
 
 authRouter.post(
     "generatePayload",
+    AppMiddleware.validateDto(GeneratePayloadDto),
     AuthController.generatePayload
 );
 
 authRouter.post(
     "signin-evm",
+    AppMiddleware.validateDto(WalletSignInDto),
     AuthController.signinEVM
 );
 
