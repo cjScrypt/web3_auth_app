5ac542ed4ec03f0067a1f11a8e9f7713bc6e73d8
Electrifier <samchijioke16@gmail.com>
2025-04-21 23:55:43 +0100

    feat: Add nonce to signin payload to prevent replay attacks

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { EvmProofService } from "./evmProofService";
+import { RedisService } from "./redis.service";
 import { TonProofService } from "./tonProof.service";
 import { UserService } from "./user.service";
 import { CHAIN_ID, CheckTonProofDto, CheckEvmProofDto } from "../types";
@@ -6,11 +7,13 @@ import { JwtUtils } from "../utils";
 
 export class AuthService {
     private readonly evmProofService: EvmProofService;
+    private readonly redisService: RedisService;
     private readonly tonProofService: TonProofService;
     private readonly userService: UserService;
 
     constructor() {
         this.evmProofService = new EvmProofService();
+        this.redisService = new RedisService();
         this.tonProofService = new TonProofService();
         this.userService = new UserService()
     }
@@ -26,7 +29,13 @@ export class AuthService {
     private async verifySigninPayload(payloadToken: string, address: string, chainId: CHAIN_ID) {
         const decoded = JwtUtils.verifyToken(payloadToken);
         if (!decoded) {
-            throw new Error("Invalid or expired token");
+            throw new Error("Invalid or expired payload");
+        }
+
+        const key = `nonce_${address}`;
+        const nonce = await this.redisService.getValue(key);
+        if (!nonce || nonce !== decoded.nonce) {
+            throw new Error("Invalid or expired payload");
         }
 
         if (decoded.address != address) {
@@ -41,7 +50,11 @@ export class AuthService {
     }
 
     async generateSigninPayload(address: string, chainId: CHAIN_ID) {
-        const payload = { address, chainId }
+        const key = `nonce_${address}`;
+        const nonce = Math.random().toString().substring(2).slice(0, 10);
+        await this.redisService.setValue(key, nonce, 15 * 60);
+
+        const payload = { address, chainId, nonce }
         const payloadToken = JwtUtils.walletProofSignature(payload);
 
         return payloadToken;
